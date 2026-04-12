10cdd5fb4a01a23be39cc0a5224663df837d1d65
SatiyaWP <satiya_wp@yahoo.com>
2026-04-12 21:54:50 +0700

    chore: add vercel serverless functions and routing config

---
+++ api/algorithms.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"net/http"
+
+	// Register all algorithms
+	_ "logicflow/internal/algorithm"
+	"logicflow/internal/handler"
+)
+
+// Handler is the Vercel serverless function entrypoint for GET /algorithms
+func Handler(w http.ResponseWriter, r *http.Request) {
+	// Let the logicflow handler process the request
+	handler.AlgorithmsHandler(w, r)
+}
